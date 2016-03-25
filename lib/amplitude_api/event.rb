5890ce05e3486d7c477b3257ed3266e4cac61e2b
Van Tran <vantran53@gmail.com>
2016-03-25 18:57:14 +0700

    Change amplitude API a little to support revenue

--- lib/amplitude_api/event.rb
+++ lib/amplitude_api/event.rb
@@ -1,34 +1,14 @@
 class AmplitudeAPI
   # AmplitudeAPI::Event
   class Event
-    # @!attribute [ rw ] user_id
-    #   @return [ String ] the user_id to be sent to Amplitude
-    attr_accessor :user_id
-    # @!attribute [ rw ] event_type
-    #   @return [ String ] the event_type to be sent to Amplitude
-    attr_accessor :event_type
-    # @!attribute [ rw ] event_properties
-    #   @return [ String ] the event_properties to be attached to the Amplitude Event
-    attr_accessor :event_properties
-    # @!attribute [ rw ] user_properties
-    #   @return [ String ] the user_properties to be passed for the user
-    attr_accessor :user_properties
-    # @!attribute [ rw ] time
-    #   @return [ Time ] Time that the event occurred (defaults to now)
-    attr_accessor :time
+    # @!attribute [ rw ] options
+    #   @return [ Hash ] the parameters to be sent to Amplitude
+    attr_accessor :options
 
     # Create a new Event
-    #
-    # @param [ String ] user_id a user_id to associate with the event
-    # @param [ String ] event_type a name for the event
-    # @param [ Hash ] event_properties various properties to attach to the event
-    # @param [ Time ] Time that the event occurred (defaults to now)
-    def initialize(user_id: '', event_type: '', event_properties: {}, user_properties: {}, time: nil)
-      self.user_id = user_id
-      self.event_type = event_type
-      self.event_properties = event_properties
-      self.user_properties = user_properties
-      self.time = time
+    def initialize(opts)
+      self.options = opts
+      self.options[:time] = formatted_time(opts[:time]) if opts[:time]
     end
 
     def user_id=(value)
@@ -44,13 +24,7 @@ class AmplitudeAPI
     #
     # Used for serialization and comparison
     def to_hash
-      serialized_event = {}
-      serialized_event[:event_type] = event_type
-      serialized_event[:user_id] = user_id
-      serialized_event[:event_properties] = event_properties
-      serialized_event[:user_properties] = user_properties
-      serialized_event[:time] = formatted_time if time
-      serialized_event
+      self.options
     end
 
     # @return [ true, false ]
@@ -66,7 +40,7 @@ class AmplitudeAPI
 
     private
 
-    def formatted_time
+    def formatted_time(time)
       time.to_i * 1_000
     end
   end
